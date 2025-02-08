7c76bc26633a2c24be5d284763e122e85ef2a3c0
henry4i20 <henrimito1305@gmail.com>
2025-02-08 15:25:16 -0300

    feat: adiciona estrutura inicial e form de cadastro

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,48 @@
+import { NavLink } from "react-router-dom";
+import styles from "./NavBar.module.css";
+const NavBar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <NavLink to={"/"} className={styles.brand}>
+        <b>FIRE</b>
+        <span>BLOG</span>
+      </NavLink>
+      <ul className={styles.links_list}>
+        <li>
+          <NavLink
+            to={"/"}
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/Login"}
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Entrar
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/register"}
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Cadastrar
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/about"}
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Sobre
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFireStore } from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHC58MOPEyeDLuXEBa8u6L_xKwkgdTDI0",
+  authDomain: "fireblog-1fb91.firebaseapp.com",
+  projectId: "fireblog-1fb91",
+  storageBucket: "fireblog-1fb91.firebasestorage.app",
+  messagingSenderId: "72576148145",
+  appId: "1:72576148145:web:803799b557349650047890",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFireStore(app);
+
+export { db };
